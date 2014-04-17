aac8b0d8a93f47e2743cba9ebc1f385beba0124f
Aaron Reeves <aaron@aaronreeves.com>
2014-04-17 14:16:04 +0100

    Updated copyright date

--- strutils.cpp
+++ strutils.cpp
@@ -8,7 +8,7 @@ Project: Atriplex Distributed Computing System
 Website: 
 Author: Aaron Reeves <aaron@aaronreeves.com>
 --------------------------------------------------
-Copyright (C) 2007 - 2008 Aaron Reeves
+Copyright (C) 2007 - 2014 Aaron Reeves
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation; either version 2 of the License, or
