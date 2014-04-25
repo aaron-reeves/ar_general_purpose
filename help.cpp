916eb82e9af541965d5efaaa1238f31136673881
Aaron Reeves <aaron@aaronreeves.com>
2014-04-25 11:03:12 +0100

    Added example to help.h/cpp

--- help.cpp
+++ help.cpp
@@ -5,6 +5,35 @@
 
 #include "help.h"
 
+void showSampleHelpMessage() {
+  CHelpItemList list;
+
+  // Leave the first part empty to display a line that starts on the left and fills both columns.
+  list.append( "", "Command line arguments:" );
+
+  // The first part will be displayed in the first column.
+  // The second part will be displayed in the second column, and will be divided and wrapped as needed.
+  // Note the manual indentation and punctuation in the first part.  This could be more automated,
+  // but at the expense of having less flexibility.
+  list.append( "  --help, -h, -?:", "Display this help message." );
+  list.append( "  --version, -v:", "Display version information." );
+
+  // Unusually long first parts will span both columns, with the second part beginning on the
+  // next line in the second column.
+  list.append( "  --database <filename>, -d <filename>:", "If --database is the only switch provided, the specified database will be opened for interactive use." );
+
+  // Show a blank line:
+  list.append( "", "" );
+
+  // Start over again in a new block:
+  list.append( "", "Interactive mode commands:" );
+  list.append( "  show tables:", "Displays a list of tables in the database, excluding Microsoft Access system tables." );
+  list.append( "  describe <tablename>:", "Describes all tables, excluding system tables." );
+
+  // Display the message.
+  printHelpList( list );
+}
+
 
 CHelpItem::CHelpItem( QString part1, QString part2 ) {
   _part1 = part1;
@@ -35,8 +64,12 @@ void printHelpList( CHelpItemList list ) {
   QStringList lines;
   QString line0;
 
-  for( i = 0; i < list.count(); ++i )
-    maxPart1Len = std::max( maxPart1Len, list[i].part1().length() );
+  // Ignore any first part with a length over 20 characters,
+  // since such items will span both columns anyway.
+  for( i = 0; i < list.count(); ++i ) {
+    if( 20 > list[i].part1().length() )
+      maxPart1Len = std::max( maxPart1Len, list[i].part1().length() );
+  }
 
   if( 20 < maxPart1Len )
     maxPart1Len = 20;
--- help.h
+++ help.h
@@ -3,6 +3,41 @@
 
 #include <QtCore>
 
+
+/* A simple class and related functions for displaying nicely formatted messages to the console.
+ *
+ * See function showSampleHelpMessage() (reproduced below) for an example.
+ *
+ * void showSampleHelpMessage() {
+ *   CHelpItemList list;
+ *
+ *   // Leave the first part empty to display a line that starts on the left and fills both columns.
+ *   list.append( "", "Command line arguments:" );
+ *
+ *   // The first part will be displayed in the first column.
+ *   // The second part will be displayed in the second column, and will be divided and wrapped as needed.
+ *   // Note the manual indentation and punctuation in the first part.  This could be more automated,
+ *   // but at the expense of having less flexibility.
+ *   list.append( "  --help, -h, -?:", "Display this help message." );
+ *   list.append( "  --version, -v:", "Display version information." );
+ *
+ *   // Unusually long first parts will span both columns, with the second part beginning on the
+ *   // next line in the second column.
+ *   list.append( "  --database <filename>, -d <filename>:", "If --database is the only switch provided, the specified database will be opened for interactive use." );
+ *
+ *   // Show a blank line:
+ *   list.append( "", "" );
+ *
+ *   // Start over again in a new block:
+ *   list.append( "", "Interactive mode commands:" );
+ *   list.append( "  show tables:", "Displays a list of tables in the database, excluding Microsoft Access system tables." );
+ *   list.append( "  describe <tablename>:", "Describes all tables, excluding system tables." );
+ *
+ *   // Display the message.
+ *   printHelpList( list );
+ * }
+ *
+ */
 class CHelpItem {
   public:
     CHelpItem( QString part1, QString part2 );
@@ -24,5 +59,7 @@ class CHelpItemList : public QList<CHelpItem> {
 };
 
 void printHelpList( CHelpItemList list );
+
+void showSampleHelpMessage();
 
 #endif // HELP_H
