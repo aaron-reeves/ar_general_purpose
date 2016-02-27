a34124f27003e184e89d05ff0f446b03b9a97cdb
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2016-02-27 11:40:58 +0000

    Added functions for calculating distances between points

--- qmath.cpp
+++ qmath.cpp
@@ -1,5 +1,6 @@
 
 #include <qmath.h>
+#include <QtCore>
 
 void inc( int& addTo ) {
   ++addTo;
@@ -27,3 +28,24 @@ bool qBetterFuzzyCompare( const double p1, const double p2){
     return (qAbs(p1 - p2) <= 0.000000000001 * qMin(qAbs(p1), qAbs(p2)));
   }
 }
+
+
+double cartesianDistanceSquared( const QPointF& p1, const QPointF& p2 ) {
+  double asq, bsq;
+
+  asq = pow( p1.x() - p2.x(), 2.0 );
+  bsq = pow( p1.y() - p2.y(), 2.0 );
+
+  return( asq + bsq );
+}
+
+
+double cartesianDistance( const QPoint& p1, const QPoint& p2 ) {
+  return sqrt( cartesianDistanceSquared( QPointF( p1.x(), p1.y() ), QPointF( p2.x(), p2.y() ) ) );
+}
+
+
+double cartesianDistance( const QPointF& p1, const QPointF& p2 ) {
+  return sqrt( cartesianDistanceSquared( p1, p2 ) );
+}
+
--- qmath.h
+++ qmath.h
@@ -1,5 +1,7 @@
 
-#include <QtGlobal>
+#include <QtCore>
+
+#include <math.h>
 
 // Adds 1 to addTo, updating its value.
 void inc( int& addTo );
@@ -13,4 +15,9 @@ void dec( int& subtractFrom );
 void dec( int& subtractFrom, const int toSubstract );
 
 bool qBetterFuzzyCompare( const double p1, const double p2);
+
+
+double cartesianDistance( const QPoint& p1, const QPoint& p2 );
+double cartesianDistance( const QPointF& p1, const QPointF& p2 );
+double cartesianDistanceSquared( const QPointF& p1, const QPointF& p2 );
 
