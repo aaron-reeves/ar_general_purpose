1acd22134fe29fe3f38bf3686de189b419d2b828
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2018-09-15 21:11:42 +0100

    New tests added

--- SampleApplications/xl2csv/main.cpp
+++ SampleApplications/xl2csv/main.cpp
@@ -27,6 +27,18 @@ int main( int argc, char* argv[] ) {
   else
     cout << csv2.error() << ": " << csv2.errorMsg() << endl << flush;
 
+
+  cout << endl << endl << "Do the new version:" << endl;
+
+  CXlCsv csv3( CXlCsv::Format2007, argv[2], true );
+
+  if( csv3.open() ) {
+    cout << csv3.asTable() << endl << flush;
+  }
+  else {
+    cout << csv3.error() << ": " << csv3.errorMsg() << endl << flush;
+  }
+
   return 0;
 }
 
