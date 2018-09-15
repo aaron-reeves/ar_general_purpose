43e8361d4a7b69378231e333b23854077042b1e0
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2018-09-15 20:35:13 +0100

    Modifications for compatibility with new spreadsheet handling classes

--- xlcsv.cpp
+++ xlcsv.cpp
@@ -4,30 +4,24 @@ xlcsv.h/cpp
 Begin: 2017/01/10
 Author: Aaron Reeves <aaron.reeves@sruc.ac.uk>
 --------------------------------------------------
-Copyright (C) 2017 Epidemiology Research Unit, Scotland's Rural College (SRUC)
+Copyright (C) 2017 - 2018 Epidemiology Research Unit, Scotland's Rural College (SRUC)
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation; either version 2 of the License, or
 (at your option) any later version.
 */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <ctype.h>
-
-#include <ar_general_purpose/xlcsv/xls.h>
-
-//#pragma pack()
+#include "cspreadsheetarray.h"
 
 #include <ar_general_purpose/qcout.h>
 
 #include "xlcsv.h"
 
+
 CXlCsv::CXlCsv() : QCsv() {
   // Accept default parameters specified by QCsv::initialize().
 
-  initializeXl();
+  initialize();
 
   // File will NOT be opened by default.
 }
@@ -40,7 +34,7 @@ CXlCsv::CXlCsv(
   const int nLinesToSkip /* = 0 */,
   const QString& sheetname /* = "" */ ) : QCsv()
 {
-  initializeXl();
+  initialize();
 
   setFileFormat( format );
   setFilename( filename );
@@ -52,9 +46,16 @@ CXlCsv::CXlCsv(
 }
 
 
-void CXlCsv::initializeXl() {
-  setSheetname( "" );
-  setFileFormat( FormatUnknown );
+void CXlCsv::initialize() {
+  QCsv::initialize();
+
+  _sheetname = "";
+  _sheetIdx = 0;
+  _useSheetname = false;
+
+  _errorOnOpen = false;
+
+  _fileFormat = FormatUnknown;
 
   setMode( QCsv::EntireFile );
 
@@ -142,148 +143,102 @@ bool CXlCsv::open() {
     return true;
   }
 
+  if( _errorOnOpen ) {
+    return false;
+  }
+
   if( FormatUnknown == _fileFormat ) {
     setError( QCsv::ERROR_OTHER, "Spreadsheet file format must be specified." );
+    _errorOnOpen = true;
     return false;
   }
 
   if( _srcFilename.isEmpty() ) {
     setError( QCsv::ERROR_OPEN, "No input file specified." );
+    _errorOnOpen = true;
     return false;
   }
 
   QFileInfo fi( _srcFilename );
   if( !fi.exists() || !fi.isReadable() ) {
     setError( QCsv::ERROR_OPEN, "Specified input file does not exist or cannot be read." );
+    _errorOnOpen = true;
     return false;
   }
 
   if( 0 > _linesToSkip ) {
     setError( ERROR_INDEX_OUT_OF_RANGE, "Number of lines to skip must be 0 or more." );
+    _errorOnOpen = true;
     return false;
   }
 
+  bool result;
+  clearError();
+
   switch ( _fileFormat ) {
     case Format97_2003:
-      return openXls();
+      result = openXls();
       break;
     case Format2007:
-      return openXlsx();
+      result = openXlsx();
       break;
     default:
       setError( QCsv::ERROR_OTHER, "Specified spreadsheet file format is not supported." );
-      return false;
+      result = false;
       break;
   }
+
+  if( !result ) {
+    _errorOnOpen = true;
+  }
+
+  return result;
 }
 
 
 bool CXlCsv::openXls() {
-  // Open workbook, choose standard conversion
-  //------------------------------------------
-  xlsWorkBook* pWB = xls_open( this->filename().toLatin1().data(), "iso-8859-15//TRANSLIT" );
+  CSpreadsheetWorkBook wb( CSpreadsheetWorkBook::Format97_2003, this->filename() );
 
-  if( NULL == pWB ) {
-    setError( QCsv::ERROR_OPEN, "Specified file could not be opened.  Wrong format?" );
-    return false;
-  }
-
-  if( 1 == pWB->is1904datesystem ) {
-    _is1904DateSystem = true;
-  }
-
-  //for( int i = 0; i < pWB->formats.count; ++i ) {
-  //  cout << "Format:" << "idx: " << pWB->formats.format[i].index << ", val: " << pWB->formats.format[i].value << endl;
-  //}
-
-  //for( int i = 0; i < pWB->xfs.count; ++i ) {
-  //  cout << "XFS: " << "idx: " << pWB->xfs.xf[i].format << ", type: " << pWB->xfs.xf[i].type << ", value: " << pWB->xfs.xf[i].format << endl;
-  //}
-
-  _sheetNames.clear();
-  for( int i = 0; i < pWB->sheets.count; ++i ) {
-    _sheetNames.append( pWB->sheets.sheet[i].name );
-  }
-
-  bool ok;
-  QString sheetToOpen = this->sheetToOpen( ok );
-  if( !ok ) {
-    xls_close( pWB );
-    _error = QCsv::ERROR_OTHER;
-    if( _useSheetname )
-      _errorMsg = QString( "Specified worksheet (%1) could not be selected." ).arg( _sheetname );
-    else
-      _errorMsg = QString( "Specified worksheet (%1) could not be selected." ).arg( _sheetIdx );
-    return false;
-  }
-
-  // Open and parse the sheet
-  //-------------------------
-  xlsWorkSheet* pWS = xls_getWorkSheet( pWB, _sheetNames.indexOf( sheetToOpen ) );
-  xls_parseWorkSheet( pWS );
-
-  // Process all rows of the sheet
-  //------------------------------
-  struct st_row_data* row;
-  XLSWORD t, tt;
-  bool firstRowFinished = false;
-  QStringList list;
-  bool lineWritten = false;
-
-  //qDebug() << "rows:" <<  pWS->rows.lastrow << ", cols:" << pWS->rows.lastcol;
-
-  for( t=0; t <= pWS->rows.lastrow; ++t ) {
-    row = &pWS->rows.row[t];
-
-    // process cells
-    if( lineWritten ) {
-      if( this->containsFieldList() && !firstRowFinished ) {
-        this->setFieldNames( list );
-        //qDebug() << "Header:" << list.count() << list;
-        firstRowFinished = true;
-      }
-      else {
-        //qDebug() << "Body:" << list.count() << list;
-        this->appendRow( list );
-      }
-      list.clear();
+  if( _useSheetname ) {
+    if( !wb.hasSheet( _sheetname ) ) {
+      setError( ERROR_OTHER, QString( "Specified worksheet (%1) could not be selected." ).arg( _sheetname ) );
+      //qDebug() << _errorMsg;
+      return false;
     }
     else {
-      lineWritten = true;
-    }
-
-    for( tt=0; tt < pWS->rows.lastcol; ++tt ) {
-      if( !row->cells.cell[tt].ishidden ) {
-        // Display the value of the cell (either numeric or string)
-        //---------------------------------------------------------
-
-        // AR: Not sure what's up with the magic constants here... Something about establishing
-        // that a cell contains a number, from the look of it.
-        // FIXME: I need to know dates!
-        if( ( 0x27E == row->cells.cell[tt].id ) || ( 0x0BD == row->cells.cell[tt].id ) || ( 0x203 == row->cells.cell[tt].id ) ) {
-           char arstr[255];
-           sprintf( arstr, "%.15g", row->cells.cell[tt].d );
-           //qDebug() << "Value:" << arstr << ", id:" << QString( "%1" ).arg( row->cells.cell[tt].id, 0, 16 ).toUpper().prepend( "0x" );
-           list.append( arstr );
-        }
-
-        // A string
-        else if( NULL != row->cells.cell[tt].str ) {
-           list.append( row->cells.cell[tt].str );
-        }
-
-        // An empty cell
-        else {
-           list.append( QString() );
-        }
-      }
+      _sheetIdx = wb.sheetIndex( _sheetname );
     }
   }
 
-  // Don't forget the last row.
-  this->appendRow( list );
+  if( !wb.hasSheet( _sheetIdx ) ) {
+    setError( ERROR_OTHER, QString( "Specified worksheet (%1) could not be selected." ).arg( _sheetIdx ) );
+    //qDebug() << _errorMsg;
+    return false;
+  }
 
-  xls_close( pWB );
+  if( !wb.readSheet( _sheetIdx ) ) {
+    setError( ERROR_OTHER, "Specified worksheet could not be read." );
+    //qDebug() << _errorMsg;
+    return false;
+  }
+
+  CSpreadsheet sheet = wb.sheet( _sheetIdx );
+
+  if( !sheet.isTidy( this->containsFieldList() ) ) {
+    setError( ERROR_OTHER, "Specified worksheet does not have a tidy CSV format." );
+    //qDebug() << _errorMsg;
+    return false;
+  }
+
+  int starti = this->_linesToSkip;
+  if( this->containsFieldList() ) {
+    this->setFieldNames( CSV::csvStringList( sheet.rowAsStringList( starti ), this->_delimiter ) );
+    ++starti;
+  }
+
+  for( int i = starti; i < sheet.nRows(); ++i ) {
+    this->appendRow( CSV::csvStringList( sheet.rowAsStringList( i ), this->_delimiter ) );
+  }
 
   _isOpen = true;
 
