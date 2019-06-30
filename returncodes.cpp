59dc80f108e6110fd282493d50d1b17a5a5999c8
Aaron Reeves <aaron.reeves@sruc.ac.uk>
2019-06-30 13:58:16 +0100

    Added new return code for network errors, and rearranged error codes so that FATAL_ERROR is the most severe (it should always be from this point onward)

--- returncodes.cpp
+++ returncodes.cpp
@@ -5,7 +5,7 @@ Begin: 2016/09/22
 Authors: Aaron Reeves <aaron.reeves@sruc.ac.uk>
          Julie Stirling <julie.stirling@sruc.ac.uk>
 ---------------------------------------------------
-Copyright (C) 2016 Epidemiology Research Unit, Scotland's Rural College (SRUC)
+Copyright (C) 2016 - 2019 Epidemiology Research Unit, Scotland's Rural College (SRUC)
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation; either version 2 of the License, or
@@ -17,27 +17,28 @@ Public License as published by the Free Software Foundation; either version 2 of
 QString ReturnCode::codeList() {
  return QString(
    "Exit codes (used in combination with one another):\n"
-   "  SUCCESS                  :      0\n"
-   "  EMPTY_INPUT_FILE         :      1\n"
-   "  DUPLICATE_RECORD         :      2\n"
-   "  KNOWN_VALIDATION_ISSUE   :      4\n"
-   "  DATA_VALIDATION_PROBLEM  :      8\n"
-   "  BAD_COMMAND              :     16\n"
-   "  INPUT_FILE_PROBLEM       :     32\n"
-   "  OUTPUT_FILE_PROBLEM      :     64\n"
-   "  ERROR_LOG_PROBLEM        :    128\n"
-   "  ERROR_VARIANT_CONVERSION :    256\n"
-   "  PROCESSING_INTERRUPTED   :    512\n"
-   "  FAILED_DB_QUERY          :   1024\n"
-   "  BAD_CONFIG               :   2048\n"
-   "  FILE_SYSTEM_PROBLEM      :   4096\n"
-   "  REQUIRED_FIELDS_MISSING  :   8192\n"
-   "  BAD_DATABASE             :  16384\n"
-   "  FATAL_ERROR              :  32768\n"
-   "  APPLICATION_ERROR        :  65536\n"
-   "  UNRECOGNIZED_FIELD       : 131072\n"
-   "  CHECKSUM_PROBLEM         : 262144\n"
-   "  UNKNOWN_RESULT           : 524288\n"
+   "  SUCCESS                  :       0\n"
+   "  EMPTY_INPUT_FILE         :       1\n"
+   "  DUPLICATE_RECORD         :       2\n"
+   "  KNOWN_VALIDATION_ISSUE   :       4\n"
+   "  DATA_VALIDATION_PROBLEM  :       8\n"
+   "  BAD_COMMAND              :      16\n"
+   "  INPUT_FILE_PROBLEM       :      32\n"
+   "  OUTPUT_FILE_PROBLEM      :      64\n"
+   "  ERROR_LOG_PROBLEM        :     128\n"
+   "  ERROR_VARIANT_CONVERSION :     256\n"
+   "  PROCESSING_INTERRUPTED   :     512\n"
+   "  FAILED_DB_QUERY          :    1024\n"
+   "  BAD_CONFIG               :    2048\n"
+   "  FILE_SYSTEM_PROBLEM      :    4096\n"
+   "  REQUIRED_FIELDS_MISSING  :    8192\n"
+   "  BAD_DATABASE             :   16384\n"
+   "  APPLICATION_ERROR        :   32768\n"
+   "  UNRECOGNIZED_FIELD       :   65536\n"
+   "  CHECKSUM_PROBLEM         :  131072\n"
+   "  UNKNOWN_RESULT           :  262144\n"
+   "  NETWORK_PROBLEM          :  524288\n"
+   "  FATAL_ERROR              : 8388608\n"
  );
 }
 
@@ -77,8 +78,6 @@ QString ReturnCode::codeDescr( const int val ) {
       result.append( "REQUIRED_FIELDS_MISSING: Check the data file.\n" );
     if( val & BAD_DATABASE )
       result.append( "BAD_DATABASE: Specified database could not be accessed.  Check your network and the specified database parameters.\n" );
-    if( val & FATAL_ERROR )
-      result.append( "FATAL_ERROR: A fatal error occurred.  Please check with the developers: this is our bad.\n" );
     if( val & APPLICATION_ERROR )
       result.append( "APPLICATION_ERROR: An application error occurred.  Please check with the developers.\n" );
     if( val & UNRECOGNIZED_FIELD )
@@ -87,7 +86,11 @@ QString ReturnCode::codeDescr( const int val ) {
       result.append( "CHECKSUM_PROBLEM: There is a mismatch between actual and expected file contents.  Please check with the developers.\n" );
     if( val & UNKNOWN_RESULT )
       result.append( "UNKNOWN_RESULT: The outcome is not yet known.  Has the process run?" );
+    if( val & NETWORK_PROBLEM )
+      result.append( "NETWORK_PROBLEM: A required network seems to be unreachable, or the required network service cannot be run.\n" );
 
+    if( val & FATAL_ERROR )
+      result.append( "FATAL_ERROR: A fatal error occurred.  Please check with the developers: this is our bad.\n" );
     return result.trimmed();
   }
 }
--- returncodes.h
+++ returncodes.h
@@ -5,7 +5,7 @@ Begin: 2016/09/16
 Authors: Aaron Reeves <aaron.reeves@sruc.ac.uk>
          Julie Stirling <julie.stirling@sruc.ac.uk>
 ---------------------------------------------------
-Copyright (C) 2016 Epidemiology Research Unit, Scotland's Rural College (SRUC)
+Copyright (C) 2016 - 2019 Epidemiology Research Unit, Scotland's Rural College (SRUC)
 
 This program is free software; you can redistribute it and/or modify it under the terms of the GNU General
 Public License as published by the Free Software Foundation; either version 2 of the License, or
@@ -20,28 +20,31 @@ Public License as published by the Free Software Foundation; either version 2 of
 // These are listed in approximate order of severity.
 namespace ReturnCode {
   enum Result {
-    SUCCESS                  = 0x00000, //      0
-    EMPTY_INPUT_FILE         = 0x00001, //      1
-    DUPLICATE_RECORD         = 0x00002, //      2
-    KNOWN_VALIDATION_ISSUE   = 0x00004, //      4
-    DATA_VALIDATION_PROBLEM  = 0x00008, //      8
-    BAD_COMMAND              = 0x00010, //     16
-    INPUT_FILE_PROBLEM       = 0x00020, //     32
-    OUTPUT_FILE_PROBLEM      = 0x00040, //     64
-    ERROR_LOG_PROBLEM        = 0x00080, //    128
-    ERROR_VARIANT_CONVERSION = 0x00100, //    256
-    PROCESSING_INTERRUPTED   = 0x00200, //    512
-    FAILED_DB_QUERY          = 0x00400, //   1024
-    BAD_CONFIG               = 0x00800, //   2048
-    FILE_SYSTEM_PROBLEM      = 0x01000, //   4096
-    REQUIRED_FIELDS_MISSING  = 0x02000, //   8192
-    BAD_DATABASE             = 0x04000, //  16384
-    FATAL_ERROR              = 0x08000, //  32768
-    APPLICATION_ERROR        = 0x10000, //  65536
-    UNRECOGNIZED_FIELD       = 0x20000, // 131072
-    CHECKSUM_PROBLEM         = 0x40000, // 262144
-    UNKNOWN_RESULT           = 0x80000  // 524288
-    //NEXT_PROBLEM = ???
+    SUCCESS                  = 0x000000, //       0
+    EMPTY_INPUT_FILE         = 0x000001, //       1
+    DUPLICATE_RECORD         = 0x000002, //       2
+    KNOWN_VALIDATION_ISSUE   = 0x000004, //       4
+    DATA_VALIDATION_PROBLEM  = 0x000008, //       8
+    BAD_COMMAND              = 0x000010, //      16
+    INPUT_FILE_PROBLEM       = 0x000020, //      32
+    OUTPUT_FILE_PROBLEM      = 0x000040, //      64
+    ERROR_LOG_PROBLEM        = 0x000080, //     128
+    ERROR_VARIANT_CONVERSION = 0x000100, //     256
+    PROCESSING_INTERRUPTED   = 0x000200, //     512
+    FAILED_DB_QUERY          = 0x000400, //    1024
+    BAD_CONFIG               = 0x000800, //    2048
+    FILE_SYSTEM_PROBLEM      = 0x001000, //    4096
+    REQUIRED_FIELDS_MISSING  = 0x002000, //    8192
+    BAD_DATABASE             = 0x004000, //   16384
+    APPLICATION_ERROR        = 0x008000, //   32768
+    UNRECOGNIZED_FIELD       = 0x010000, //   65536
+    CHECKSUM_PROBLEM         = 0x020000, //  131072
+    UNKNOWN_RESULT           = 0x040000, //  262144
+    NETWORK_PROBLEM          = 0x080000, //  524288
+    //NEXT_PROBLEM        // = 0x100000, // 1048576
+                          // = 0x200000, // 2097152
+                          // = 0x400000, // 4194304
+    FATAL_ERROR              = 0x800000  // 8388608
     //...
   };
 
